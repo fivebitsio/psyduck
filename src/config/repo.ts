6049befe846c5c1d50fd2696b09ca5722d9785d1
Praveen Kumar <polurupraveen@gmail.com>
2025-08-27 00:34:30 +0530

    uncomment lowdb misunderstanding

--- src/config/repo.ts
+++ src/config/repo.ts
@@ -1,14 +1,6 @@
 import { Low } from 'lowdb'
 import { ConfigSchema, User } from './types'
 
-/*
-  Lowdb doesn't support concurrency.
-  Concurrent writes will corrupt the data,
-  Since changing config is an admin thing
-  and only done as a rare maintenance work,
-  this is fine
-*/
-
 function createConfigRepo(db: Low<ConfigSchema>) {
   async function addUser(user: User): Promise<void> {
     await db.update(({ users }) => users.push(user))
