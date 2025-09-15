a944be6004ed479a00b1f7fcd4bf50a675b876cd
Praveen Kumar <876188+poluruprvn@users.noreply.github.com>
2025-09-15 13:36:01 +0530

    adds login ui (#6)
    * adds login ui
    
    * adds biome github action
    
    * adds biome github action

---
+++ web/src/pages/auth/index.tsx
@@ -0,0 +1,52 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+function LoginForm() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login to your account</CardTitle>
+              <CardDescription>
+                Enter your username below to login to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="username">Username</Label>
+                    <Input name="username" placeholder="psyduck" required />
+                  </div>
+                  <div className="grid gap-3">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">Password</Label>
+                    </div>
+                    <Input name="password" type="password" required />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
