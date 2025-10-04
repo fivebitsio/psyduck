cccf5707a8a1cb98596b1dc4b0e98563b3babf79
Praveen Kumar <polurupraveen@gmail.com>
2025-10-04 12:05:31 +0530

    handle concurrent duckdb access

--- src/analytics/repo.ts
+++ src/analytics/repo.ts
@@ -1,4 +1,7 @@
 import type { DuckDBConnection } from '@duckdb/node-api'
+import { Mutex } from 'async-mutex'
+import * as ct from 'countries-and-timezones'
+import * as queries from './queries'
 import type {
   Metric,
   precision,
@@ -9,27 +12,30 @@ import type {
   VisitsByPage,
   VisitsBySource,
 } from './types'
-import * as ct from 'countries-and-timezones'
-import * as queries from './queries'
 
 function createAnalyticsRepo(duckdb: DuckDBConnection) {
+
+  const mutex = new Mutex()
+
   async function getVisits(
     from: string,
     to: string,
     precision: precision,
   ): Promise<Metric[]> {
-    const result = await duckdb.runAndReadAll(queries.visits, {
-      from,
-      to,
-      precision,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visits, {
+        from,
+        to,
+        precision,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        time: row.time?.toString() ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      time: row.time?.toString() ?? '',
-      count: Number(row.count),
-    }))
   }
 
   async function getPageViews(
@@ -37,17 +43,19 @@ function createAnalyticsRepo(duckdb: DuckDBConnection) {
     to: string,
     precision: precision,
   ): Promise<Metric[]> {
-    const result = await duckdb.runAndReadAll(queries.pageViews, {
-      from,
-      to,
-      precision,
-    })
-    const rows = result.getRowObjects()
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.pageViews, {
+        from,
+        to,
+        precision,
+      })
+      const rows = result.getRowObjects()
 
-    return rows.map(row => ({
-      time: row.time?.toString() ?? '',
-      count: Number(row.count),
-    }))
+      return rows.map(row => ({
+        time: row.time?.toString() ?? '',
+        count: Number(row.count),
+      }))
+    })
   }
 
   async function getVisitsByCountry(
@@ -55,71 +63,79 @@ function createAnalyticsRepo(duckdb: DuckDBConnection) {
     to: string,
     limit: number,
   ): Promise<VisitsByCountry[]> {
-    const result = await duckdb.runAndReadAll(queries.visitsByCountry, {
-      from,
-      to,
-      limit,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visitsByCountry, {
+        from,
+        to,
+        limit,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        countryCode: row.country_code?.toString() ?? '',
+        countryName:
+          ct.getCountry(row.country_code?.toString() ?? '')?.name ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      countryCode: row.country_code?.toString() ?? '',
-      countryName:
-        ct.getCountry(row.country_code?.toString() ?? '')?.name ?? '',
-      count: Number(row.count),
-    }))
   }
 
   async function getVisitsByDeviceType(
     from: string,
     to: string,
   ): Promise<VisitsByDeviceType[]> {
-    const result = await duckdb.runAndReadAll(queries.visitsByDeviceType, {
-      from,
-      to,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visitsByDeviceType, {
+        from,
+        to,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        deviceType: row.deviceType?.toString() ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      deviceType: row.deviceType?.toString() ?? '',
-      count: Number(row.count),
-    }))
   }
 
   async function getVisitsByBrowser(
     from: string,
     to: string,
   ): Promise<VisitsByBrowser[]> {
-    const result = await duckdb.runAndReadAll(queries.visitsByBrowser, {
-      from,
-      to,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visitsByBrowser, {
+        from,
+        to,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        browser: row.browser?.toString() ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      browser: row.browser?.toString() ?? '',
-      count: Number(row.count),
-    }))
   }
 
   async function getVisitsByOs(
     from: string,
     to: string,
   ): Promise<VisitsByOs[]> {
-    const result = await duckdb.runAndReadAll(queries.visitsByOs, {
-      from,
-      to,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visitsByOs, {
+        from,
+        to,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        os: row.os?.toString() ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      os: row.os?.toString() ?? '',
-      count: Number(row.count),
-    }))
   }
 
   async function getVisitsByPage(
@@ -127,35 +143,39 @@ function createAnalyticsRepo(duckdb: DuckDBConnection) {
     to: string,
     limit: number = 10,
   ): Promise<VisitsByPage[]> {
-    const result = await duckdb.runAndReadAll(queries.visitsByPage, {
-      from,
-      to,
-      limit,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visitsByPage, {
+        from,
+        to,
+        limit,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        pathname: row.pathname?.toString() ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      pathname: row.pathname?.toString() ?? '',
-      count: Number(row.count),
-    }))
   }
 
   async function getVisitsBySource(
     from: string,
     to: string,
   ): Promise<VisitsBySource[]> {
-    const result = await duckdb.runAndReadAll(queries.visitsBySource, {
-      from,
-      to,
+    return await mutex.runExclusive(async () => {
+      const result = await duckdb.runAndReadAll(queries.visitsBySource, {
+        from,
+        to,
+      })
+
+      const rows = result.getRowObjects()
+
+      return rows.map(row => ({
+        source: row.referrer?.toString() ?? '',
+        count: Number(row.count),
+      }))
     })
-
-    const rows = result.getRowObjects()
-
-    return rows.map(row => ({
-      source: row.referrer?.toString() ?? '',
-      count: Number(row.count),
-    }))
   }
 
   return {
