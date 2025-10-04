95967a0962205da10ef748af508d93513de2156c
Praveen Kumar <polurupraveen@gmail.com>
2025-10-04 12:15:53 +0530

    cleanup unused code

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,12 +3,15 @@ import { Route, Switch } from 'wouter'
 import Analytics from '@/pages/analytics'
 import LoginForm from '@/pages/auth'
 import Settings from '@/pages/settings'
+import AuthProtected from './components/auth-protected'
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" component={Analytics} />
-      <Route path="/settings" component={Settings} />
+      <AuthProtected>
+        <Route path="/" component={Analytics} />
+        <Route path="/settings" component={Settings} />
+      </AuthProtected>
       <Route path="/login" component={LoginForm} />
     </Switch>
   )
